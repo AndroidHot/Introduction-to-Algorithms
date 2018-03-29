bb70f9fa9f56f10e3318823dc5cd768a008b6ca3
AndroidHot <zhiwei.dev@gmail.com>
2018-03-29 22:17:50 +0800

    OverView:Change the insert function of a binary search tree.
    Description:
        modified:   chapter12/binary_search_tree.py

--- chapter12/binary_search_tree.py
+++ chapter12/binary_search_tree.py
@@ -7,33 +7,30 @@ class BinarySearchTree(object):
     left = 0
     right = 1
 
-    def __init__(self, root_node):
-        if not isinstance(root_node, BinarySearchTreeNode):
+    def __init__(self, root_node = None):
+        if root_node != None and not isinstance(root_node, BinarySearchTreeNode):
             raise ValueError('root_node is not BinarySearchTreeNode, please check!')
         self.root_node = root_node
         self._sip_node_parent = None
 
-    def insert(self, node):
+    def tree_insert(self, node):
         if not isinstance(node, BinarySearchTreeNode):
             raise ValueError('insert node is not BinarySearchTreeNode, please check!')
-        node_parent, direction = self._search_insertion_position(node, self.root_node)
-        if direction == self.left:
-            node_parent.left = node
-            node.parent = node_parent
-        elif direction == self.right:
-            node_parent.right = node
-            node.parent = node_parent
-
-    def _search_insertion_position(self, node, compare_node, direction=None):
-        if compare_node == None:
-            return self._sip_node_parent, direction
-        self._sip_node_parent = compare_node
-        if node.key <= compare_node.key:
-            # search left childen
-            return self._search_insertion_position(node, compare_node.left, self.left)
+        y = None
+        x = self.root_node
+        while x != None:
+            y = x
+            if node.key <= x.key:
+                x = x.left
+            else:
+                x = x.right
+        node.parent = y
+        if y == None:
+            self.root_node = node    # tree was empty
+        elif node.key <= y.key:
+            y.left = node
         else:
-            # search right childen
-            return self._search_insertion_position(node, compare_node.right, self.right)
+            y.right = node
 
     def tree_search(self, node, value):
         if node == None or value == node.key:
