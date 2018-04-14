460e0408469e7e1ec022ed31626b791f8297c401
AndroidHot <zhiwei.dev@gmail.com>
2018-04-14 17:27:09 +0800

    OverView:Completed Exercise 15.4-3
    Description:
        modified:   chapter15/longest_common_subsequence.py

--- chapter15/longest_common_subsequence.py
+++ chapter15/longest_common_subsequence.py
@@ -26,6 +26,25 @@ def lcs_length(X, Y):
                 b[i][j] = LEFT
     return c, b
 
+# Exercise 15.4-3 top-down with memoization
+def memoized_lcs_length(X, Y):
+    m = len(X)
+    n = len(Y)
+    c = np.full((m, n), -1)
+    return lookup_lcs_length(X, Y, c, m - 1, n - 1)
+
+def lookup_lcs_length(X, Y, c, i, j):
+    if c[i][j] > -1:
+        return c[i][j]
+    if i == -1 or j == -1:
+        c[i][j] = 0
+    else:
+        if X[i] == Y[j]:
+            c[i][j] = lookup_lcs_length(X, Y, c, i - 1, j - 1) + 1
+        else:
+            c[i][j] = max(lookup_lcs_length(X, Y, c, i - 1, j), lookup_lcs_length(X, Y, c, i, j - 1))
+    return c[i][j]
+
 # Construction longest common subsequence.
 def print_lcs(b, X, i, j):
     if i == -1 or j == -1:
@@ -45,3 +64,4 @@ if __name__ == '__main__':
     print(c)
     print(b)
     print_lcs(b, X, len(X) - 1, len(Y) - 1)
+    print(memoized_lcs_length(X, Y))
